c2b2f13456e7ed7561d41384e71a27debe57d57b
Richard-Netto <richard.v.s.netto@gmail.com>
2023-09-06 01:18:52 -0300

    fix: Updated App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i) as HTMLElement;
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders example-repo link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/example-repo/i) as HTMLElement;
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should render main App', () => {
+    render(<App />);
+  });
 });
