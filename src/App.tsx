c02e63dc17fa392bb1e6f3301ac2303d645b215c
Richard Vaz da Silva Netto <richard.v.s.netto@gmail.com>
2023-09-08 02:07:45 -0300

    feature: Created TextInput component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 // Assets
 import AppLogo from './components/AppLogo/AppLogo';
+import TextInput from './components/TextInput/TextInput';
 
 const App = (): JSX.Element => {
   return (
     // App Container
     <div className="text-center">
-      <div className="flex max-h-screen min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-800 text-lg text-white">
+      <div className="flex max-h-screen min-h-screen flex-col items-center justify-center overflow-hidden bg-[#051D3B] text-lg text-white">
         <AppLogo />
+
+        <br />
+
+        <TextInput />
       </div>
     </div>
   );
